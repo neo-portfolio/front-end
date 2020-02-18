e7087f86124c097473bdebc2fa205734f92a4302
Alexandre Senges <alexandre@senges.ch>
2020-02-18 16:53:50 +0100

    Dockerfile + .dockerignore

--- src/components/home/context.tsx
+++ src/components/home/context.tsx
@@ -2,10 +2,10 @@ import {Context, createContext} from "react";
 import {Company} from "@interfaces/company";
 import {Query, Weight} from "@interfaces/neo4j";
 
-interface ContextData {
-	companies: Query<Company>,
+export interface ContextData {
+	companies: Query<Company>[],
 	weights: Weight[]
-	setCompanies: (newCompanies: Query<Company>) => void;
+	setCompanies: (newCompanies: Query<Company>[]) => void;
 	setWeights: (newWeights: Weight[]) => void;
 }
 
--- src/components/portfolio/index.tsx
+++ src/components/portfolio/index.tsx
@@ -1,15 +1,21 @@
 import React, {useContext} from "react";
-import {HomeContext} from "@components/home/context";
+import {ContextData, HomeContext} from "@components/home/context";
 import {RightContainer} from "@components/home/rightContainer";
+import {Query} from "@interfaces/neo4j";
+import {Company} from "@interfaces/company";
+import {CompanyDisplay} from "./companyDisplay";
 
 export default () => {
-	const {companies} = useContext(HomeContext);
-	// Using react hooks https://reactjs.org/docs/hooks-reference.html#usecontext
-	// With context https://reactjs.org/docs/context.html
+    const {companies}: { companies: Query<Company>[] } = useContext<ContextData>(HomeContext);
+    console.log(companies);
+    // Using react hooks https://reactjs.org/docs/hooks-reference.html#usecontext
+    // With context https://reactjs.org/docs/context.html
 
-	return (
-		<RightContainer>
-			{JSON.stringify(companies)}
-		</RightContainer>
-	);
+    const companyArray: Company[] = companies.map((q: Query<Company>) => q.row).flat();
+
+    return (
+        <RightContainer>
+            {companyArray && companyArray.map((company: Company) => <CompanyDisplay {...company} />)}
+        </RightContainer>
+    );
 }
