313528f6459f87772a2f6533deef580200e2b9ff
BenInTheBox <benoit.fontannaz@gmail.com>
2020-02-23 21:47:09 +0100

    weights dispay

--- src/components/stats/index.tsx
+++ src/components/stats/index.tsx
@@ -1,13 +1,36 @@
-import {HomeContext} from "@components/home/context";
+import {ContextData, HomeContext} from "@components/home/context";
 import React, {useContext} from "react";
 import {RightContainer} from "@components/home/rightContainer";
+import {Weight} from "@interfaces/neo4j";
+import {WeightDisplay} from "./weightsDisplay";
+import Styled from "styled-components";
+
+const CompanyStyle = Styled.section`
+    background-color: azure;
+    opacity: 1;
+    border-radius: 5px;
+    border: 2px solid black;
+    overflow-y: auto;
+    margin-bottom: 5px;
+`;
+
+const TitleStyle = Styled.div`
+    margin-bottom: 2px;
+    text-align: center;
+    font-style: italic;
+    font-family: "Times New Roman", Times, serif;
+    font-weight: bolder;
+`;
 
 export default () => {
-	const {weights} = useContext(HomeContext);
+	const {weights}: {weights: Weight[]} = useContext<ContextData>(HomeContext);
 
 	return (
 		<RightContainer>
-            {JSON.stringify(weights)}
+			{weights && <CompanyStyle>
+				{weights && <TitleStyle>Portfolio weights</TitleStyle>}
+				{weights && weights.map((weight) => <WeightDisplay {...weight} />)}
+			</CompanyStyle>}
 		</RightContainer>
 	);
 }
